fcf5a9878f8831990188a6d72cca1d57710d6ef6
Cesarinh <160445469+Cesarinh@users.noreply.github.com>
2024-06-07 11:49:36 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cristobala4/firebase_options.dart';
+import 'package:cesar1060/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
